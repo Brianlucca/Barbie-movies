9ebf72deb943db62e9547e467b3119b8d3b7e847
Brianlucca <brianlucca2002@gmail.com>
2024-03-30 12:38:41 -0300

    feat: added the transition component to the home.

--- src/screen/home-page/index.jsx
+++ src/screen/home-page/index.jsx
@@ -1,9 +1,11 @@
 import Header from '../../components/header';
+import TransitioImages from '../../components/transition-images';
 
 function Home() {
   return (
     <>
       <Header />
+      <TransitioImages />
     </>
   );
 }
